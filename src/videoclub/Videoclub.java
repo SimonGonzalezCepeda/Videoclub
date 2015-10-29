7813838b550ea558624049ea10880a399d36703e
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-10-29 15:51:16 +0100

    Creats explicacions de les funcions i comentaris. Ha aparegut private.xml

--- src/videoclub/Videoclub.java
+++ src/videoclub/Videoclub.java
@@ -14,27 +14,58 @@ public class Videoclub {
     public Usuari usuari;
     public Lloguer lloguer;
     
+    /*
+    *   Constructor
+    */
+    
     public Videoclub(){
         usuari = new Usuari();
         lloguer = new Lloguer();
     }
     
+    /*
+    *   Funcuions.
+    */
+    
+    /*
+    *   Métode per a cercar els continguts de l'aplicació.
+    *
+    */
+    
     public void buscarLloguer(){
         
     }
+    
+    /*
+    *   Mètode per a que els usuaris es puguin conectar.
+    *
+    */
     
     public void loguejar(){
         
     }
     
+    /*
+    *   Mètode amb el que es carrega tota l'informació en variables.
+    *
+    */
+    
     public void carregarBD(){
         
     }
     
+    /*
+    *   Mètode amb el que es desa tota l'informació en variables.
+    *
+    */
     public void desarBD(){
         
     }
     
+    /*
+    *   Mètode amb el que filtrem els continguts de les búsquedes.
+    *
+    */
     public void filtrarContingut(){
         
     }
