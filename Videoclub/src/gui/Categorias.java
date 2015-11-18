a4d1531ce91843a4e2b90003ae501a26d592b694
pfores <paufores@iesebre.com>
2015-11-18 17:59:25 +0100

    Part grafica un pel mes avançada (v1.1)

--- Videoclub/src/gui/Categorias.java
+++ Videoclub/src/gui/Categorias.java
@@ -35,11 +35,11 @@ public class Categorias extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 601, Short.MAX_VALUE)
+            .addGap(0, 488, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 499, Short.MAX_VALUE)
+            .addGap(0, 420, Short.MAX_VALUE)
         );
 
         pack();
--- Videoclub/src/gui/MenuPrincipal.java
+++ Videoclub/src/gui/MenuPrincipal.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package gui;
+import javax.swing.JLabel;
 import videoclub.Videoclub;
 
 
@@ -12,6 +13,7 @@ import videoclub.Videoclub;
  * @author pau
  */
 public class MenuPrincipal extends javax.swing.JFrame {
+    private Object jLabel1;
 
     /**
      * Creates new form MenuPrincipal
@@ -37,57 +39,81 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(102, 0, 51));
+
+        jScrollPane1.setBackground(new java.awt.Color(0, 0, 255));
 
         jTextPane1.setEditable(false);
-        jTextPane1.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
+        jTextPane1.setBackground(new java.awt.Color(51, 51, 51));
+        jTextPane1.setBorder(null);
+        jTextPane1.setFont(new java.awt.Font("Dialog", 0, 36)); // NOI18N
+        jTextPane1.setForeground(new java.awt.Color(204, 255, 0));
         jTextPane1.setText("Videoclub");
         jScrollPane1.setViewportView(jTextPane1);
 
+        jButton1.setBackground(new java.awt.Color(204, 204, 0));
+        jButton1.setForeground(new java.awt.Color(0, 0, 0));
         jButton1.setText("Alquilar");
 
+        jButton2.setBackground(new java.awt.Color(204, 204, 0));
+        jButton2.setForeground(new java.awt.Color(0, 0, 0));
         jButton2.setText("Categorias");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
+        jButton3.setBackground(new java.awt.Color(204, 204, 0));
+        jButton3.setForeground(new java.awt.Color(0, 0, 0));
         jButton3.setText("Mi Perfil");
 
+        jButton4.setBackground(new java.awt.Color(204, 204, 0));
+        jButton4.setForeground(new java.awt.Color(0, 0, 0));
         jButton4.setText("Desconectar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(249, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(228, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(228, 228, 228))
+                .addGap(192, 192, 192))
             .addGroup(layout.createSequentialGroup()
-                .addGap(82, 82, 82)
+                .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 105, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(44, 44, 44)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(42, 42, 42)
-                .addComponent(jButton1)
                 .addGap(29, 29, 29)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(41, 41, 41)
+                .addComponent(jButton1)
+                .addGap(36, 36, 36)
                 .addComponent(jButton2)
-                .addGap(33, 33, 33)
+                .addGap(36, 36, 36)
                 .addComponent(jButton3)
-                .addGap(33, 33, 33)
+                .addGap(36, 36, 36)
                 .addComponent(jButton4)
-                .addContainerGap(112, Short.MAX_VALUE))
+                .addContainerGap(128, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:    
+    }//GEN-LAST:event_jButton2ActionPerformed
+    
     /**
+     *
      * @param args the command line arguments
      */
     public static void main(String args[]) {
@@ -130,4 +156,5 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
+
 }
